df1147851995e329b26e6d8e9f6ec2f240029a12
jiajidi <jiajidi@163.com>
2016-01-22 11:22:03 +0800

    LFW

---
+++ myDeepIDCaffe/LFW/lfwDeepid.py
@@ -0,0 +1,75 @@
+import caffe
+import os
+import numpy as np
+from caffe import layers as L
+from caffe import params as P
+from shutil import rmtree
+
+def protoConstructor(srcPath, batchSize):
+    # net define
+    myNet = caffe.NetSpec()
+    # Layer: input
+    myNet.data, myNet.label = L.Data(batch_size = batchSize, backend = P.Data.LMDB, source = srcPath, transform_param = dict(scale = 1./255), ntop = 2)
+    # Layer: conv-maxpool-1st
+    myNet.conv1 = L.Convolution(myNet.data, kernel_size = 4, num_output = 20, weight_filler = dict(type='xavier'))
+    myNet.pool1 = L.Pooling(myNet.conv1, kernel_size = 2, stride = 2, pool = P.Pooling.MAX)
+    # Layer: conv-maxpool-2nd
+    myNet.conv2 = L.Convolution(myNet.pool1, kernel_size = 3, num_output = 40, weight_filler = dict(type='xavier'))
+    myNet.pool2 = L.Pooling(myNet.conv2, kernel_size = 2, stride = 2, pool = P.Pooling.MAX)
+    # Layer: conv-maxpool-3rd
+    myNet.conv3 = L.Convolution(myNet.pool2, kernel_size = 3, num_output = 60, weight_filler = dict(type='xavier'))
+    myNet.pool3 = L.Pooling(myNet.conv3, kernel_size = 2, stride = 2, pool = P.Pooling.MAX)
+    # Layer: conv-4th
+    myNet.conv4 = L.Convolution(myNet.pool3, kernel_size = 2, num_output = 80, weight_filler = dict(type='xavier'))
+    # Layer: full connect
+    myNet.fcconv4 = L.InnerProduct(myNet.conv4, num_output = 160, weight_filler = dict(type='xavier'))
+    myNet.fcpool3 = L.InnerProduct(myNet.pool3, num_output = 160, weight_filler = dict(type='xavier'))
+    # Layer: deep id, use eltwise to sum up, then regularize
+    myNet.rawdeepid = L.Eltwise(myNet.fcconv4, myNet.fcpool3, eltwise_param = {'operation': 1})
+    myNet.deepid = L.ReLU(myNet.rawdeepid)
+    # Layer: loss
+    myNet.fullconnect = L.InnerProduct(myNet.deepid, num_output = 4000, weight_filler = dict(type='xavier'))
+    myNet.loss = L.SoftmaxWithLoss(myNet.fullconnect, myNet.label)
+    # return the prototxt
+    return myNet.to_proto()
+
+def solverConstructor(filename, encounter):
+    solveParas = dict(train_net='"./lfw_deepid_' + str(encounter) + '.prototxt"', base_lr=0.050, weight_decay=0.001,\
+        lr_policy='"inv"', gamma=0.001, power=0.75, display=10, solver_mode="GPU", snapshot=500,\
+        snapshot_prefix='"./snapshot_lfw_' + str(encounter) + '/"')
+    with open(filename, 'w') as f:
+        for key, val in solveParas.iteritems():
+            f.write('{}: {}\n'.format(key, val))
+    return filename
+
+def dash(solver, predictLayer, N):
+    #print [(k, v.data.shape) for k, v in solver.net.blobs.items()]
+    #print [(k, v[0].data.shape) for k, v in solver.net.params.items()]
+    train_interval = 100
+    # the main solver loop
+    for it in range(N):
+        # accuracy
+        correct = 0
+        for i in xrange(10):
+            solver.net.forward()
+            correct += (solver.net.blobs[predictLayer].data.argmax(1) == solver.net.blobs['label'].data).sum()
+        print 'Train: [%04d]: %04d/1000=%.5f%%' % (it, correct, correct/10.0)
+        # SGD by Caffe
+        solver.step(train_interval)
+
+
+if __name__ == '__main__':
+    caffe.set_mode_gpu()
+    caffe.set_device(1)
+    for i in range(60):
+        print 'Dashing...', i
+        try:
+            rmtree('./snapshot_lfw_' + str(i) + '/')
+        except:
+            pass
+        os.mkdir('./snapshot_lfw_' + str(i) + '/')
+        with open('./lfw_deepid_' + str(i) + '.prototxt', 'w') as f:
+            f.write(str(protoConstructor('./lmdb_' + str(i), 100)))
+        print ' Net construct ok.'
+        mySolver = caffe.SGDSolver(solverConstructor('deepidSolver' + str(i) + '.prototxt', i))
+        dash(mySolver, 'fullconnect', 100)
---
+++ myDeepIDCaffe/LFW/myvalid.py
@@ -0,0 +1,200 @@
+import caffe
+import os
+import sys
+import numpy as np
+from sklearn import svm
+from sklearn.preprocessing import normalize
+
+def get_2partdata(filename, transformer, encounter):
+    x = []
+    y = []
+    with open(filename, 'r') as f:
+        n = int(f.readline().strip())
+        # the same one
+        for label in range(n):
+            print '\rList: %d/%d'%(label, 2*n),
+            name, num1, num2 = f.readline().strip().split('\t')
+            try:
+                img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num1)) + str(encounter) + '.jpg')))
+                img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num2)) + str(encounter) + '.jpg')))
+                x.extend([img1, img2])
+                y.append(1)
+            except:
+                print 'Image not exist', name, num1, num2
+                pass
+            # flip part
+            try:
+                if (encounter >= 30 and encounter <= 35):
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num1)) + str(encounter + 6) + '.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num2)) + str(encounter + 6) + '.jpg')))
+                    x.extend([img1, img2])
+                    y.append(1)
+                elif (encounter >= 36 and encounter <= 41):
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num1)) +  str(encounter - 6) + '.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num2)) + str(encounter - 6) + '.jpg')))
+                    x.extend([img1, img2])
+                    y.append(1)
+                elif (encounter >= 48 and encounter <= 53):
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num1)) + str(encounter + 6) + '.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num2)) + str(encounter + 6) + '.jpg')))
+                    x.extend([img1, img2])
+                    y.append(1)
+                elif (encounter >= 54 and encounter <= 59):
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num1)) + str(encounter - 6) + '.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num2)) + str(encounter - 6) + '.jpg')))
+                    x.extend([img1, img2])
+                    y.append(1)
+                else:
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num1)) + str(encounter) + '_flip.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name, 'pat_%s_%04d_'%(name, int(num2)) + str(encounter) + '_flip.jpg')))
+                    x.extend([img1, img2])
+                    y.append(1)
+            except:
+                print 'Image not exist', name, num1, num2
+                pass
+            sys.stdout.flush()
+        # the diff face
+        for label in range(n):
+            print '\rList: %d/%d'%(n+label, 2*n),
+            name1, num1, name2, num2 = f.readline().strip().split('\t')
+            try:
+                img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name1, 'pat_%s_%04d_'%(name1, int(num1)) + str(encounter) + '.jpg')))
+                img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name2, 'pat_%s_%04d_'%(name2, int(num2)) + str(encounter) + '.jpg')))
+                x.extend([img1, img2])
+                y.append(0)
+            except:
+                print 'Image not exist', name1, num1, name2, num2
+                pass
+            # flip part
+            try:
+                if (encounter >= 30 and encounter <= 35):
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name1, 'pat_%s_%04d_'%(name1, int(num1)) + str(encounter + 6) + '.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name2, 'pat_%s_%04d_'%(name2, int(num2)) + str(encounter + 6) + '.jpg')))
+                    x.extend([img1, img2])
+                    y.append(0)
+                elif (encounter >= 36 and encounter <= 41):
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name1, 'pat_%s_%04d_'%(name1, int(num1)) + str(encounter - 6) + '.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name2, 'pat_%s_%04d_'%(name2, int(num2)) + str(encounter - 6) + '.jpg')))
+                    x.extend([img1, img2])
+                    y.append(0)
+                elif (encounter >= 48 and encounter <= 53):
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name1, 'pat_%s_%04d_'%(name1, int(num1)) + str(encounter + 6) + '.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name2, 'pat_%s_%04d_'%(name2, int(num2)) + str(encounter + 6) + '.jpg')))
+                    x.extend([img1, img2])
+                    y.append(0)
+                elif (encounter >= 54 and encounter <= 59):
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name1, 'pat_%s_%04d_'%(name1, int(num1)) + str(encounter - 6) + '.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name2, 'pat_%s_%04d_'%(name2, int(num2)) + str(encounter - 6) + '.jpg')))
+                    x.extend([img1, img2])
+                    y.append(0)
+                else:
+                    img1 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name1, 'pat_%s_%04d_'%(name1, int(num1)) + str(encounter) + '_flip.jpg')))
+                    img2 = transformer.preprocess('data', caffe.io.load_image(os.path.join('/home/share/mylfw/', name2, 'pat_%s_%04d_'%(name2, int(num2)) + str(encounter) + '_flip.jpg')))
+                    x.extend([img1, img2])
+                    y.append(0)
+            except:
+                print 'Image not exist: ', name1, num1, name2, num2
+                pass
+            sys.stdout.flush()
+    return np.array(x), np.array(y)
+
+def get_feature(net, x):
+    print 'Getting feature of data whose shape is {}'.format(x.shape)
+    # deepid
+    net.blobs['data'].reshape(*((x.shape[0], ) + net.blobs['data'].data.shape[1:]))
+    net.blobs['data'].data[...] = x
+    net.forward()
+    return net.blobs['deepid'].data
+
+def test(clf, trainx, trainy, testx, testy, **kwargs):
+    clf.fit(trainx, trainy)
+    acc = (clf.predict(trainx, **kwargs) == trainy).sum()
+    print 'Train: {}/{}'.format(acc, len(trainy))
+    acc = (clf.predict(testx, **kwargs) == testy).sum()
+    print 'Test: {}/{}'.format(acc, len(testy))
+
+def preprocess():
+    transformerVector = []
+    netVector = []
+    for i in range(60):
+        if i < 30:
+            net = caffe.Net('./lfwvalid.deploy.prototxt', './snapshot_lfw_' + str(i) + '/_iter_9500.caffemodel', caffe.TEST)
+        else:
+            net = caffe.Net('./lfwvalid2.deploy.prototxt', './snapshot_lfw_' + str(i) + '/_iter_9500.caffemodel', caffe.TEST)
+        transformer = caffe.io.Transformer({'data': net.blobs['data'].data.shape})
+        transformer.set_transpose('data', (2,0,1))
+        transformer.set_raw_scale('data', 1.0)  # the reference model operates on images in [0,255] range instead of [0,1]
+        transformer.set_channel_swap('data', (2,1,0))  # the reference model has channels in BGR order instead of RGB
+        netVector.append(net)
+        transformerVector.append(transformer)
+    return netVector, transformerVector
+
+class Threshold(object):
+    def __init__(self):
+        pass
+
+    def fit(self, x, y, tol=1e-5):
+        sortedx = sorted(x)
+        l = 0.0
+        r = 1.0
+        while l+tol < r:
+            lmid = (2*l+r)/3.0
+            rmid = (l+2*r)/3.0
+            if (self.predict(x, lmid)==y).sum() <= (self.predict(x, rmid)==y).sum():
+                l = lmid
+            else:
+                r = rmid
+        self.p = (l+r)/2.0 
+
+    def predict(self, x, p=None):
+        if p == None: p = self.p
+        ind = int(x.shape[0]*p)
+        if ind == x.shape[0]: ind -= 1
+        threshold = sorted(x)[ind]
+        y = np.zeros((x.shape[0],))
+        y[x >= threshold] = 1
+        return y
+
+if __name__ == '__main__':
+    netVec, transVec = preprocess()
+    trainyVec = []
+    testyVec = []
+    trainFea = []
+    testFea = []
+    onceflag = False
+    for i in range(60):
+        print 'Round:', i
+        trainx, trainy = get_2partdata('./lfw_rules/pairsDevTrain.txt', transVec[i], i)
+        testx, testy = get_2partdata('./lfw_rules/pairsDevTest.txt', transVec[i], i)
+        trainx = get_feature(netVec[i], trainx).reshape(trainy.shape[0], 2, -1) 
+        testx = get_feature(netVec[i], testx).reshape(testy.shape[0], 2, -1)
+        _trainx = normalize(trainx[:, 0, :] - trainx[:, 1, :])
+        _testx = normalize(testx[:, 0, :] - testx[:, 1, :])
+
+        for j in range(len(trainFea), len(_trainx)):
+            trainFea.append([])
+        for j in range(len(testFea), len(_testx)):
+            testFea.append([])
+        for j in range(0, len(_trainx)):
+            trainFea[j].extend(_trainx[j])
+        for j in range(0, len(_testx)):
+            testFea[j].extend(_testx[j])
+        if onceflag == False:
+            trainyVec.extend(trainy)
+            testyVec.extend(testy)
+            onceflag = True
+
+    from sklearn import svm
+    print 'SVM Linear:'
+    clf = svm.SVC(kernel='linear', probability=True, verbose=False, max_iter=50000)
+    test(clf, trainFea, trainyVec, testFea, testyVec)
+    print 'Cosdiff:'
+    nptrainx = np.array(trainFea)
+    nptrainy = np.array(trainyVec)
+    nptestx = np.array(testFea)
+    nptesty = np.array(testyVec)
+
+    itrainx = (nptrainx[:, 0, :] * nptrainx[:, 1, :]).sum(1)/np.sqrt((nptrainx[:, 0, :]**2).sum(1))/np.sqrt((nptrainx[:, 1, :]**2).sum(1))
+    itestx = (nptestx[:, 0, :] * nptestx[:, 1, :]).sum(1)/np.sqrt((nptestx[:, 0, :]**2).sum(1))/np.sqrt((nptestx[:, 1, :]**2).sum(1))
+    clf = Threshold()
+    test(clf, itrainx, trainyVec, itestx, testyVec)
